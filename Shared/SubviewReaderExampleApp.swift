b13a54eaea9f863d10ebebee74544605cc893e79
Piyush Priyadarshi <piyush@PIYUSHs-MacBook-Pro-2.local>
2022-03-21 15:54:11 +0530

    first commit

---
+++ Shared/SubviewReaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubviewReaderExampleApp.swift
+//  Shared
+//
+//  Created by Piyush Priyadarshi on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SubviewReaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SubviewReader/SubviewReader.h
@@ -0,0 +1,18 @@
+//
+//  SubviewReader.h
+//  SubviewReader
+//
+//  Created by Piyush Priyadarshi on 21/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubviewReader.
+FOUNDATION_EXPORT double SubviewReaderVersionNumber;
+
+//! Project version string for SubviewReader.
+FOUNDATION_EXPORT const unsigned char SubviewReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubviewReader/PublicHeader.h>
+
+
